de73580520802a478e08ec21ca16a85e58183140
DebsTD22 <dominguetidebora@gmail.com>
2025-06-24 22:56:12 -0300

    funções para receber dados

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,6 +2,8 @@
 
 int main(){
 
+    // declaração das variaveis
+
     char estado;
     char codCarta[20]; 
     char nomeCid[50];
@@ -10,5 +12,26 @@ int main(){
     float pib;
     int pontosTurist;
 
+    // recebendo os dados da primeira carta
+
+    printf("Digite o estado da carta de 'A' ate 'H': ");
+    scanf("%c", &estado);
+
+    printf("Digite o nome da cidade: ");
+    scanf("%s", &nomeCid);
+
+    printf("Qual a população: ");
+    scanf("%d", &populacao);
+
+    printf("Sua área total: ");
+    scanf("%f", &area);
+
+    printf("O PIB dessa cidade: ");
+    scanf("%f", &pib);
+
+    printf("Quantos pontos turisticos tem a cidade: ");
+    scanf("%d", &pontosTurist);
+
+    // lendo e mostrando os dados da primeira carta
 
 }
